ce59fef740c6185e8a18fd30f45a3d0ec8756725
HanShin <vlrkwlepd@gmail.com>
2015-10-06 13:03:41 +0900

    ^^

---
+++ DailyReport/DB/DBCommon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DailyReport.DB
+{
+    public class DBCommon
+    {
+        private string connectionString;
+        public string ConnectionString 
+        {
+            get
+            {
+                return connectionString;
+            }
+        }
+    }
+}
